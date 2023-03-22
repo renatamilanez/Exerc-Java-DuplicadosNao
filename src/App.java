fe543aa2c86c63ecb3e115ff85d69478cc396647
Renata Milanez <renatamilanez.dev@gmail.com>
2023-03-22 17:28:03 -0300

    refactor: changed for ArrayList

--- src/App.java
+++ src/App.java
@@ -1,14 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        String[] array1 = new String[]{"Morango", "Uva", "Acerola", "Manga"};
-        String[] array2 = new String[]{"Pêra", "Caju", "Morango", "Kiwi", "Acerola"};
+        String[] array1 = new String[]{"Morango", "Uva", "Acerola", "Manga", "Banana", "Mamão"};
+        String[] array2 = new String[]{"Pêra", "Caju", "Morango", "Kiwi", "Acerola", "Uva"};
+        List<String> result = new ArrayList<>();
 
         for(int i=0; i < array1.length; i++){
             for(int j=0; j < array2.length; j++){
                 if(array1[i] == array2[j]){
-                    System.out.println(array1[i]);
+                    result.add(array1[i]);
                 }
             }
         }
+        System.out.println(result);
     }
 }
